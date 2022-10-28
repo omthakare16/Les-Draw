ba15b96b9d62a84d7eb278957001749ccdd50d85
Om Thakare <omthakare1606@gmail.com>
2022-10-28 22:00:55 +0530

    added catpuccin rainbow effect

--- src/script.js
+++ src/script.js
@@ -1,12 +1,34 @@
 const container = document.getElementById("container");
+const catppuccin = ['#f4dbd6', '#f0c6c6', '#f5bde6', '#c6a0f6', '#ed8796', '#ee99a0', '#f5a97f', '#eed49f', '#a6da95', '#8bd5ca', '#91d7e3', '#7dc4e4', '#8aadf4', '#b7bdf8', '#cad3f5', '#b8c0e0', '#6e738d', '#494d64', '#24273a', '#1e2030', '#181926']
+
+
+function getCatColor() {
+  const catppuccin = ['#f4dbd6', '#f0c6c6', '#f5bde6', '#c6a0f6', '#ed8796', '#ee99a0', '#f5a97f', '#eed49f', '#a6da95', '#8bd5ca', '#91d7e3', '#7dc4e4', '#8aadf4', '#b7bdf8', '#cad3f5', '#b8c0e0', '#6e738d', '#494d64', '#24273a', '#1e2030', '#181926']
+
+  var item = catppuccin[Math.floor(Math.random() * catppuccin.length)];
+  return item;
+}
+
 
 // ---------------- Features for the sketch boards -----------------------------------------------
 document.getElementById("cleaner").addEventListener("click", (e) => {
-  kolor = "";
+  let colr_col = document.querySelectorAll(".col");
+  colr_col.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+
+      kolor = "";
+    });
+  });
 });
 
 document.getElementById("pen").addEventListener("click", (e) => {
-  kolor = memCol;
+  let colr_col = document.querySelectorAll(".col");
+  colr_col.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+
+      kolor = memCol;
+    });
+  });
 });
 
 document.getElementById("clear").addEventListener("click", (e) => {
@@ -20,10 +42,25 @@ document.getElementById("clear").addEventListener("click", (e) => {
 });
 
 
+document.getElementById("catppuccin").addEventListener("click", (e) => {
+  let colr_col = document.querySelectorAll(".col");
+  colr_col.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+
+      kolor = getCatColor()
+    });
+  });
+});
+
+
+
+
+
 // ---------------- grid function-----------------------------------------------
 function gridGen(valSize) {
   container.innerHTML = "";
   // console.log();
+  j = 0;
   for (let index = 0; index < valSize; index++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -31,6 +68,8 @@ function gridGen(valSize) {
       col = document.createElement("div");
       row.appendChild(col);
       col.classList.add("col");
+      col.style.innerHTML = j;
+      j++;
     }
     container.appendChild(row);
   }
@@ -53,12 +92,14 @@ function gridGen(valSize) {
 // ---------------- grid resizer-----------------------------------------------
 let size = document.getElementById("size");
 // console.log(size);
+valSize = "16x16";
 size.addEventListener("change", (e) => {
   // console.log(e.target.value);
   valSize = e.target.value;
   gridGen(valSize);
-});
 
+  document.getElementById("size-val").innerHTML = "Grid Size <br> " + valSize + " X " + valSize
+});
 // ---------------- default colour + colour picker -----------------------------------------------
 let memCol = "#cc80ff";
 let kolor = "#cc80ff";
